4284196a9a2c30eba6bae054d16e301f3c98a939
aluno <aluno@lab2-pc06.local>
2019-05-16 21:57:11 -0300

    incluido cliente

---
+++ ProjetoModelo/src/main/java/br/edu/ifc/projeto/sistema/model/Cliente.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifc.projeto.sistema.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private Date dtnascimento;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(Long id, String nome, Date dtnascimento, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.dtnascimento = dtnascimento;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDtnascimento() {
+        return dtnascimento;
+    }
+
+    public void setDtnascimento(Date dtnascimento) {
+        this.dtnascimento = dtnascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+    
+    
+    
+}
